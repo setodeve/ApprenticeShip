b915946f38e4ca029a9b5f27de4e2d5800f3fe6c
setodeve <keinick11@outlook.com>
2023-10-19 23:57:41 +0900

    edit vending machine

--- Ruby/level2/OOP/vendingmachine.rb
+++ Ruby/level2/OOP/vendingmachine.rb
@@ -1,14 +1,34 @@
 class VendingMachine
   def initialize(s)
     @str = s
+    @money = 0
   end
-  def press_manufacturer_name
-    return @str
+  
+  def deposit_coin(m)
+    if m==100
+      @money = m
+    end
   end
+
   def press_button
-    return "cider"
+    if(@money>=100) 
+      return "cider"
+    end
   end
+
+  private
+    def press_manufacturer_name
+      return @str
+    end
 end
 
 vending_machine = VendingMachine.new('サントリー')
+puts vending_machine.press_button
+
+vending_machine.deposit_coin(150)
+puts vending_machine.press_button
+
+vending_machine.deposit_coin(100)
+puts vending_machine.press_button
+
 puts vending_machine.press_manufacturer_name
