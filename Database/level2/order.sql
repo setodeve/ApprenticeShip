8a9115bb47414ed8d7d4a3070cb8f159be8d2a2a
setodeve <keinick11@outlook.com>
2023-11-01 00:22:43 +0900

    add order

---
+++ Database/level2/order.sql
@@ -0,0 +1,3 @@
+select * from employees order by birth_date limit 10 ;
+select * from employees order by birth_date desc limit 10 ;
+select * from employees order by birth_date desc, hire_date desc limit 30 ;
