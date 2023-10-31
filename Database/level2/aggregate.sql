a8eb922d61ce14f4268b6f2fd5ddf92021398161
setodeve <keinick11@outlook.com>
2023-11-01 00:33:45 +0900

    add aggregate

---
+++ Database/level2/aggregate.sql
@@ -0,0 +1,8 @@
+select sum(salary) from salaries ;
+select avg(salary) from salaries ;
+select round(avg(salary)) from salaries ;
+select max(salary) from salaries ;
+select min(salary) from salaries ;
+select count(salary) from salaries ;
+select max(salary) from salaries where from_date = "1986-06-26" ;
+select avg(salary) from salaries where from_date = "1991-06-26" ;
