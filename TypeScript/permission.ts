2d30ec892b75acb5f60d0f0540b56be6ef73b5de
setodeve <keinick11@outlook.com>
2023-11-16 02:45:42 +0900

    add eslint and pretter

--- TypeScript/permission.ts
+++ TypeScript/permission.ts
@@ -1,35 +1,38 @@
 interface User {
-  username: string,
-  permissions: any
+  username: string;
+  permissions: {
+    canRead: boolean;
+    canWrite: boolean;
+    canDelete: boolean;
+  };
 }
 
-let users:User[] = [
+const users: User[] = [
   {
-    username: '山田',
+    username: "山田",
     permissions: {
       canRead: true,
       canWrite: true,
-      canDelete: false
-    }
+      canDelete: false,
+    },
   },
   {
-    username: '佐藤',
+    username: "佐藤",
     permissions: {
       canRead: false,
       canWrite: true,
-      canDelete: false
-    }
+      canDelete: false,
+    },
   },
-
 ];
 
-function checkPermission(username:string, permission:string) {
-  for (let i=0;i<users.length;i++){
-    if (users[i]["username"]==username){
-      console.log(users[i]["permissions"][permission])
+function checkPermission(username: string, permission: string) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]["username"] == username) {
+      console.log(users[i]["permissions"][permission]);
     }
   }
 }
 
-checkPermission('山田', 'canWrite')
-checkPermission('佐藤', 'canDelete')
+checkPermission("山田", "canWrite");
+checkPermission("佐藤", "canDelete");
