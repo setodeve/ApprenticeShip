6fb01249758bd181271d9b02a80f60a69639869e
setodeve <keinick11@outlook.com>
2023-11-16 02:06:05 +0900

    add permission

---
+++ TypeScript/permission.ts
@@ -0,0 +1,35 @@
+interface User {
+  username: string,
+  permissions: any
+}
+
+let users:User[] = [
+  {
+    username: '山田',
+    permissions: {
+      canRead: true,
+      canWrite: true,
+      canDelete: false
+    }
+  },
+  {
+    username: '佐藤',
+    permissions: {
+      canRead: false,
+      canWrite: true,
+      canDelete: false
+    }
+  },
+
+];
+
+function checkPermission(username:string, permission:string) {
+  for (let i=0;i<users.length;i++){
+    if (users[i]["username"]==username){
+      console.log(users[i]["permissions"][permission])
+    }
+  }
+}
+
+checkPermission('山田', 'canWrite')
+checkPermission('佐藤', 'canDelete')
