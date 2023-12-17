3524a30ddff1fda8bacb7c1e501c9c6d38a8a1ca
setodeve <keinick11@outlook.com>
2023-12-18 00:36:21 +0900

    Article初期設定追加

--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   resources :users
-  # resources :articles
   # root "articles#index"
 end
--- Rails/db/schema.rb
+++ Rails/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_17_093014) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_17_153030) do
+  create_table "articles", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
