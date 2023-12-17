5a01b82b03a9589de9bacd12b31f6bf436026ded
setodeve <keinick11@outlook.com>
2023-12-17 18:34:08 +0900

    User初期設定追加

--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :users
+  # resources :articles
   # root "articles#index"
 end
