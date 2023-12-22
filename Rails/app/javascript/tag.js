2bd32a6f76aecc72222884289992588cbe26ae41
setodeve <keinick11@outlook.com>
2023-12-22 23:27:03 +0900

    タグ消去機能

--- Rails/app/javascript/tag.js
+++ Rails/app/javascript/tag.js
@@ -1,19 +1,33 @@
 const tagInput = document.getElementById('tag-input')
 const tagList = document.getElementById('tag-list')
-tagInput.addEventListener("keypress", (e) => AddTag(e));
-tagInput.removeEventListener("keypress", (e) => AddTag(e));
+const tagEvent = document.getElementsByClassName('tag-event')
+tagInput.addEventListener("keypress", (e) => addTag(e));
+tagInput.removeEventListener("keypress", (e) => addTag(e));
 
-function AddTag(e){
+if (tagEvent != undefined) {
+  for(let i = 0; i < tagEvent.length; i++) {addTagEvent(tagEvent[i])}
+}
+
+
+function addTag(e){
   if (e.key === "Enter") {
     e.preventDefault();
     const info = e.target.value.split(',')
-    tagInput.value = ""
-    tagList.innerHTML += `
-      <span class="tag-default tag-pill"> 
-        <i class="ion-close-round"></i> 
-        ${info[1]}
-        <input name="article[tag_ids][]" value=${info[0]} autocomplete="off" type="hidden" id="tag_ids">
-      </span>
+    const spanTag = document.createElement('span')
+    spanTag.classList.add("tag-default","tag-pill","tag-event")
+    addTagEvent(spanTag)
+    spanTag.innerHTML = `
+      <i class="ion-close-round"></i> 
+      ${info[1]}
+      <input name="article[tag_ids][]" value=${info[0]} autocomplete="off" type="hidden" id="tag_ids">
     `
+    tagList.appendChild(spanTag)
+    tagInput.value = ""
   }
+}
+
+function addTagEvent(element){
+  element.addEventListener("click", () => {
+    element.parentNode.removeChild(element)
+  });
 }
